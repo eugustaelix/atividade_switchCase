a1abde22b1a7c4e189afdc9b561f75de5afc5ab7
eugustaelix <160174940+eugustaelix@users.noreply.github.com>
2024-05-09 21:00:07 -0300

    Add files via upload

--- script.php
+++ script.php
@@ -1,22 +1,34 @@
-<body>
-    <div>
-        <?php
-        $favcolor = $_POST["favcolor"];
-        // Utilizando o switch case para determinar a cor favorita
+<?php
+$favcolor = $_POST["favcolor"];
+// Utilizando o switch case para determinar a cor favorita
 
-        switch ($favcolor) {
-            case "vermelho":
-                echo "<p>Sua cor favorita é vermelhol</p>";
-                break;
-            case "azul":
-                echo "<p>Sua cor favorita é azull</p>";
-                break;
-            case "verde":
-                echo "<p>Sua cor favorita é verde!</p>";
-                break;
-            default:
-                echo "<p>Sua cor favorita não é vermelho, azul ou verdel </p>";
-        }
-        ?>
-    </div>
+switch ($favcolor) {
+    case "vermelho":
+        $resultado = "Sua cor favorita é vermelho!";
+        break;
+    case "azul":
+        $resultado = "Sua cor favorita é azul!";
+        break;
+    case "verde":
+        $resultado = "Sua cor favorita é verde!";
+        break;
+    default:
+        $resultado = "Sua cor favorita não é vermelho, azul ou verde!";
+}
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Feito por: Gustavo Felix Ferreira da Silva & Nayara Aparecida Silva Fioraneli — 2MD2 -->
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Cor favorita</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+<body>
+<a href="index.html"></a>
+<label><?php echo $resultado ?></label>
 </body>
+
+</html>
